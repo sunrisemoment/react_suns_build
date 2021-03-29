27eb2ffe501a5e630593627a6f4c48106789e0eb
Jin Zhao <sunrisemoment@outlook.com>
2021-03-30 06:35:39 +0800

    commit for test terraform script on aws s3

---
+++ precache-manifest.5e33721c883d2b666884bc74dee88f1b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8234448de21c1436f1fda61f59940a04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "157958edab71758dbf89",
+    "url": "/static/css/2.d5e4bcd6.chunk.css"
+  },
+  {
+    "revision": "4fc92c8f68b964bb609a",
+    "url": "/static/css/main.76358415.chunk.css"
+  },
+  {
+    "revision": "157958edab71758dbf89",
+    "url": "/static/js/2.8d08be85.chunk.js"
+  },
+  {
+    "revision": "15f1e809c2e074d57cafc193285c7926",
+    "url": "/static/js/2.8d08be85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fc92c8f68b964bb609a",
+    "url": "/static/js/main.b8bc0455.chunk.js"
+  },
+  {
+    "revision": "c5e259d7555de5f18575",
+    "url": "/static/js/runtime-main.239d88f9.js"
+  },
+  {
+    "revision": "50c63ebc60e4f6f502dd09b59c55146b",
+    "url": "/static/media/BebasNeue-Bold.50c63ebc.ttf"
+  },
+  {
+    "revision": "54413459a5adf3f82686db50595cba5a",
+    "url": "/static/media/BebasNeue-Regular.54413459.ttf"
+  },
+  {
+    "revision": "c1102669a2d73f63ea382dc8fd825319",
+    "url": "/static/media/ar_icon_trans.c1102669.png"
+  },
+  {
+    "revision": "f2b02f36986ffde67cad7a79dbceb43d",
+    "url": "/static/media/court_2.f2b02f36.jpg"
+  },
+  {
+    "revision": "9a8c18bd1dbe8508bc2525be7e07d0ff",
+    "url": "/static/media/helvetica-light.9a8c18bd.ttf"
+  }
+]);
